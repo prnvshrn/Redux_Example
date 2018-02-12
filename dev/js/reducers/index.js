6084225f3236cae1e03e76f70c6f5c1333b75d4b
prnvshrn <prnvshrn@gmail.com>
2018-02-11 17:25:01 -0700

    Changed reducer and store

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,6 +1,5 @@
 import {combineReducers} from 'redux';
 import UserReducer from './reducer-users';
-import ActiveUserReducer from './reducer-active-user';
 
 /*
  * We combine all reducers into a single object before updated data is dispatched (sent) to store
@@ -9,7 +8,6 @@ import ActiveUserReducer from './reducer-active-user';
 
 const allReducers = combineReducers({
     users: UserReducer,
-    activeUser: ActiveUserReducer
 });
 
 export default allReducers
